9ac2d06775fa9d126f737ffe73031955891869cc
Bhumika <hi.bhumikasinha@gmail.com>
2025-10-04 19:59:29 -0500

    Beta version

--- src/components/EarthSection.tsx
+++ src/components/EarthSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const EarthSection: React.FC = () => {
   return (
-    <section className="py-20 bg-gray-900">
+    <section className="py-20 bg-black-900">
       <div className="max-w-7xl mx-auto px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="rounded-lg overflow-hidden bg-black">
